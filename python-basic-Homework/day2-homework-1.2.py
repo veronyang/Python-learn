3de84ff4bd271dd05c1b48338b65759f7af0cc7d
veronyang <53370737@qq.com>
2026-04-03 22:24:49 +0800

    Update syslog formatting in day2-homework-1.1.py to enhance clarity and consistency

---
+++ python-basic-Homework/day2-homework-1.2.py
@@ -0,0 +1,16 @@
+"""
+切片：提取接口类型和编号
+现在有一个接口名字符串:
+
+interface = "GigabitEthernet0/0/1"
+通过切片的方式，分别提取出接口类型和接口编号，并打印:
+
+接口类型: GigabitEthernet
+接口编号: 0/0/1
+提示: 先数一下 "GigabitEthernet" 有几个字符。
+"""
+
+interface = "GigabitEthernet0/0/1"
+print('接口类型：'+'interface[:-5]')
+print('接口编号：'+'interface[-5:]')
+
